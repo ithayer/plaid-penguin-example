bace090de8d0d31b07555af07de46a94a20c712e
Ignacio Thayer <ignacio@cookie.(none)>
2011-06-04 23:42:04 -0700

    Update README

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]
-		 [org.clojars.ithayer/plaid-penguin "1.0.0"]]
+		 [org.clojars.ithayer/plaid-penguin "1.0.0"]
+		 [org.clojars.ithayer/thrift "0.6.1"]
+		 [org.clojars.kjw/slf4j "1.5.5"]
+		 [org.clojars.kjw/slf4j-simple "1.5.5"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
 		     [lein-javac "1.2.1-SNAPSHOT"]]
   :java-source-path [["gen-java/"]]
